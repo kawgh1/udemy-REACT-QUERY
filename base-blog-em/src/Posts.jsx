c158ca2bbac2a65e48a3590f18f7d1bf5b93f170
kawgh1 <krseven@gmail.com>
2022-03-16 09:24:25 -0500

    got pagination working

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -6,17 +6,17 @@ import { useQuery } from "react-query";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
-    const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
-    );
-    return response.json();
-}
+// async function fetchPosts() {
+//     const response = await fetch(
+//         "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+//     );
+//     return response.json();
+// }
 
-const fetchPosts2 = async () => {
+const fetchPosts2 = async (pageNum) => {
     try {
         const response = await axios.get(
-            "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+            `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
         );
         return response.data;
     } catch (error) {
@@ -25,7 +25,7 @@ const fetchPosts2 = async () => {
 };
 
 export function Posts() {
-    const [currentPage, setCurrentPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
     const [selectedPost, setSelectedPost] = useState(null);
 
     // replace with useQuery
@@ -33,9 +33,16 @@ export function Posts() {
     // useQuery takes a user supplied key name, "posts", and an async function that returns the data as a promise
     // the base react query has a bunch of properties we can destructure off of and use in our code
     // like isError and isLoading
-    const { data, isError, error, isLoading } = useQuery("posts", fetchPosts2, {
-        staleTime: 2000,
-    });
+    //
+    // notice we are using state variables as parameters in our queries
+
+    const { data, isError, error, isLoading } = useQuery(
+        ["posts", currentPage],
+        () => fetchPosts2(currentPage),
+        {
+            staleTime: 2000,
+        }
+    );
     if (isLoading) return <h3>Loading...</h3>;
     if (isError)
         return (
@@ -59,11 +66,21 @@ export function Posts() {
                 ))}
             </ul>
             <div className="pages">
-                <button disabled onClick={() => {}}>
+                <button
+                    disabled={currentPage <= 1}
+                    onClick={() => {
+                        setCurrentPage((previousValue) => previousValue - 1);
+                    }}
+                >
                     Previous page
                 </button>
-                <span>Page {currentPage + 1}</span>
-                <button disabled onClick={() => {}}>
+                <span>Page {currentPage}</span>
+                <button
+                    disabled={currentPage >= maxPostPage}
+                    onClick={() => {
+                        setCurrentPage((previousValue) => previousValue + 1);
+                    }}
+                >
                     Next page
                 </button>
             </div>
